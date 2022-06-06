991eb993a3b4869d4f8316a862204ac0436c51b6
João Ferreira <joaovtf39@gmail.com>
2022-06-06 00:16:44 -0300

    rerun avaliator

--- src/connection.ts
+++ src/connection.ts
@@ -6,9 +6,6 @@ dotenv.config();
 // const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
 const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
 
-const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI
-    || MONGO_DB_URL,
-) => mongoose.connect(mongoDatabaseURI);
+const connectToDatabase = () => mongoose.connect(MONGO_DB_URL);
 
 export default connectToDatabase;
